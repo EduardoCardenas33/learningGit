a7c33d780324770b542ac3006ffe06d8ca6fa3e4
EduardoCardenas33 <jian.cardenas@pucp.pe>
2023-07-18 20:54:29 +0200

    Adding mathematics

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -6,5 +6,11 @@ int main()
 {
     cout << "Hello world!" << endl;
     cout << "I love testing new things" <<endl;
+
+    double f = 5.0;
+    double f2 = 4.5;
+    double dd = f + f2;
+
+    std::cout<< dd<<endl;
     return 0;
 }
